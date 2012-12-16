b339ce76d4e48f25529eff4d3ba364631c7a2534
raz.barak@gmail.com <raz.barak@gmail.com@c773a91c-5486-69a0-4503-52e213ce5d2b>
2012-12-16 07:43:41 +0000

    My First working app

---
+++ trunk/ServicesDemo/src/com/example/MyService.java
@@ -0,0 +1,143 @@
+package com.example;
+
+import java.text.Format;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Service;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.widget.Toast;
+import android.provider.CalendarContract;
+
+public class MyService extends Service {
+	private static final String TAG = "MyService";
+	
+	TimerTask mTimerTask;
+	final Handler handler = new Handler();
+	Timer t = new Timer();
+
+	AudioManager mAudioManager;
+	private int nCounter = 0;
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+	
+	@Override
+	public void onCreate() {
+		Toast.makeText(this, "My Service Created", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "onCreate");
+		
+		mAudioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+	}
+
+	@Override
+	public void onDestroy() {
+		Toast.makeText(this, "My Service Stopped", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "onDestroy");
+		stopTask();
+	}
+	
+	@Override
+	public void onStart(Intent intent, int startid) {
+		Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "onStart");
+		doTimerTask();
+	}
+	
+	 public void doTimerTask(){
+			
+		mTimerTask = new TimerTask() {
+			public void run() {
+				handler.post(new Runnable() {
+					public void run() {
+						nCounter++;
+						Log.d("TIMER", "TimerTask run " + nCounter);
+						amIFree();
+					}
+				});
+			}
+		};
+ 
+			// public void schedule (TimerTask task, long delay, long period)
+
+		int periodInMins = 1;
+	    Calendar c = Calendar.getInstance();
+	    	    
+	    GregorianCalendar gc = new GregorianCalendar (c.get(Calendar.YEAR),
+	    		c.get(Calendar.MONTH),
+	    		c.get(Calendar.DAY_OF_MONTH),
+	    		c.get(Calendar.HOUR_OF_DAY),
+	    		periodInMins - (c.get(Calendar.MINUTE)%periodInMins) + c.get(Calendar.MINUTE));
+	    
+	    Log.d("TIMER", gc.getTime().toString());
+	    
+	    t.schedule(mTimerTask, gc.getTime(),periodInMins*60*1000);
+		amIFree();
+	}
+ 
+	public void stopTask(){ 
+		if(mTimerTask!=null){ 
+			Log.d("TIMER", "timer canceled");
+			mTimerTask.cancel();
+		}
+	}
+	
+	private void amIFree(){
+		Cursor mCursor = getContentResolver().query(
+				Uri.parse("content://com.android.calendar/events"),
+				new String[]{ "calendar_id", "title", "description", "dtstart", "dtend", "eventLocation", "availability"},
+				null,
+				null,
+				CalendarContract.Events.DTSTART + " ASC");
+		
+		mCursor.moveToFirst();
+		
+		long ntime = System.currentTimeMillis();
+		String[] CalNames = new String[mCursor.getCount()];
+		int[] CalIds = new int[mCursor.getCount()];  
+		int i;
+		for (i = 0; i < CalNames.length; i++) {  
+			CalIds[i] = mCursor.getInt(0);             
+			CalNames[i] = "Event"+mCursor.getInt(0)+": \nTitle: "+ mCursor.getString(1)+"\nDescription: "+mCursor.getString(2)+"\nStart Date: "+mCursor.getLong(3)+"\nEnd Date : "+mCursor.getLong(4)+"\nLocation : "+mCursor.getString(5);  
+			long StartTime = mCursor.getLong(3);  
+			long EndTime = mCursor.getLong(4);  
+			//do compare here  
+			//if we are on the middle of something stop checking and leave the loop  
+			if ((StartTime<ntime)&&(ntime<EndTime)&&mCursor.getInt(6)==CalendarContract.Events.AVAILABILITY_BUSY) {
+				Format df = DateFormat.getDateFormat(this);
+				Format tf = DateFormat.getTimeFormat(this);
+				String title;
+				long start;
+				try {
+					title = mCursor.getString(1);
+					start = mCursor.getLong(3);
+					System.out.println("CalendarID - " + mCursor.getString(0)+ " " +title+" on "+df.format(start)+" at "+tf.format(start) + " Avaliability "+ mCursor.getInt(6));
+					System.out.println("In the middle of something");
+					
+					mAudioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+					
+					Log.d("TIMER", "In the middle of something\n" + "CalendarID - " + mCursor.getInt(0)+ " " +title+" on "+df.format(start)+" at "+tf.format(start));
+				} catch (Exception e) {
+				//ignore
+				}				
+				break;  
+			}
+			mCursor.moveToNext();  
+		}
+		if (i==CalNames.length){
+			Log.d("TIMER", "The Calendar is Free");
+			mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+		}
+	}
+}
---
+++ trunk/ServicesDemo/src/com/example/ServicesDemo.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ServicesDemo extends Activity implements OnClickListener {
+  private static final String TAG = "ServicesDemo";
+  Button buttonStart, buttonStop;
+  
+  int myCounter = 0;
+  protected View handler;
+  Timer myt = new Timer();  
+  TimerTask myTimerTask;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    buttonStart = (Button) findViewById(R.id.buttonStart);
+    buttonStop = (Button) findViewById(R.id.buttonStop);
+
+    buttonStart.setOnClickListener(this);
+    buttonStop.setOnClickListener(this);
+        
+  }
+  
+  public void onClick(View src) {
+    switch (src.getId()) {
+    case R.id.buttonStart:
+      Log.d(TAG, "onClick: starting srvice");
+      startService(new Intent(this, MyService.class));
+      break;
+    case R.id.buttonStop:
+      Log.d(TAG, "onClick: stopping srvice");
+      stopService(new Intent(this, MyService.class));
+      break;
+    }
+  }
+}
